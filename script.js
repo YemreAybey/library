419f416ee1059fe8f1b28e6b81b532535fc0a991
Yemre <aybey77@gmail.com>
2019-09-11 12:59:22 +0300

    Linter

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-function Book(title, author, isRead, pages){
+function Book(title, author, isRead, pages) {
   this.title = title;
   this.author = author;
   this.isread = isRead;
   this.pages = pages;
 }
 
-const book1 =new Book("Yemre", "Yemre", "Unread", 255);
-const book2 = new Book("Afani", "Afani", "Unread", 122);
-const book3 =new Book("Ebuka", "Ebuka", "Unread", 245);
+const book1 = new Book('Yemre', 'Yemre', 'Unread', 255);
+const book2 = new Book('Afani', 'Afani', 'Unread', 122);
+const book3 = new Book('Ebuka', 'Ebuka', 'Unread', 245);
 
 const books = [book1, book2, book3];
 
@@ -15,16 +15,16 @@ function addBook(book) {
   books.push(book);
 }
 
-function removeBook(book){
+function removeBook(book) {
   books.splice(book, 1);
 }
 
-function deleteItem(book){
+function deleteItem(book) {
   book.target.parentNode.parentNode.remove();
   removeBook(book.target.id);
 }
 
-const render = function(node, book){
+const render = (node, book) => {
   const template = `
   <td>${book.title}</td>\
   <td>${book.author}</td>\
@@ -33,55 +33,52 @@ const render = function(node, book){
   <td><button type="button" id="${books.indexOf(book)}" class="btn btn-danger delete">DELETE</button></td>\
   `;
   node.innerHTML += template;
-  const dButton =   document.querySelectorAll("button.delete");
-  dButton.forEach(btn => btn.addEventListener('click', deleteItem));
+  const dButton = document.querySelectorAll('button.delete');
+  dButton.forEach (btn => btn.addEventListener('click', deleteItem));
 
-  const readBtn = document.querySelectorAll("button.read");
-  readBtn.forEach(btn => btn.addEventListener('click', function(){
-  if(book.isread == "Read"){
-      book.isread = "Unread";
+  const readBtn = document.querySelectorAll('button.read');
+  readBtn.forEach (btn => btn.addEventListener('click', () => {
+    if (book.isread === 'Read') {
+      book.isread = 'Unread';
       btn.innerHTML = `${book.isread}`;
-  }
-  else {
-      book.isread = "Read";
+    }
+    else {
+      book.isread = 'Read';
       btn.innerHTML = `${book.isread}`;
-  }
+    }
   }));
 };
 
-for (let book of books){
+for (let book of books) {
   render(document.querySelector('#books'), book);
-}
-    
-const openForm = function() {
-  const formArea = document.getElementById("formarea");
-  formArea.classList.toggle("d-none");
+}  
+const openForm = () => {
+  const formArea = document.getElementById('formarea');
+  formArea.classList.toggle('d-none');
 };
-const addBtn = document.querySelector("button.addButton");
+const addBtn = document.querySelector('button.addButton');
 addBtn.addEventListener('click', openForm);
 
-const saveButton = document.querySelector("button.save");
-const closeButton = document.querySelector("button.cancel");
+const saveButton = document.querySelector('button.save');
+const closeButton = document.querySelector('button.cancel');
 closeButton.addEventListener('click', openForm);
 
 function addAndRenderBook() {
   const bookParts = document.querySelectorAll("input[type='text']");
-  const radio = document.getElementById("true");
-  const isread = "Read";
+  const radio = document.getElementById('true');
+  let isread = 'Read';
   if (radio.checked) {
-    isread = "Unread";
+    isread = 'Unread';
   }
 
-const book = new Book(bookParts[0].value, bookParts[1].value, isread, bookParts[2].value );
-for (let i in bookParts) {
-  if (bookParts[i].value == "" ){
-    alert("Please Fill The Form Properly");
-    return;
+  const book = new Book(bookParts[0].value, bookParts[1].value, isread, bookParts[2].value);
+  for (let i of bookParts) {
+    if (i.value === '') {
+      alert('Please Fill The Form Properly');
+      return;
+    }
   }
-}
   addBook(book);
   render(document.querySelector('#books'), book);   
 }
 saveButton.addEventListener('click', addAndRenderBook);
-
-
